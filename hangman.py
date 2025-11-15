30af4c0c839a542f900bfd6d4ec28db12360136b
Buggy Burrow <cassy@buggyburrow.com>
2025-11-15 16:14:24 +0900

    add functions to display word and to prompt for letters

--- hangman.py
+++ hangman.py
@@ -1,5 +1,27 @@
 def main():
-    print("Welcome to Buggy Hangman.")
+    criminal_name = "Bob"
+
+    print(f"Welcome to Buggy Hangman. Let's hang {criminal_name}")
+
+    secret_word = "buggy"
+    guesses = []
+
+def prompt_char () -> str:
+    while True:
+        user_input = input("Choose a letter: ")
+        if user_input and user_input[0].isalpha():
+            return user_input[0].lower()
+
+
+def display_word(word: str, guesses: list):
+
+    for char in word:
+        if char in guesses:
+            print(f"{char}", end=" ")
+        else:
+            print("_", end=" ")
+    
+    print()
 
 if __name__ == "__main__":
     main()
